c6b7d52811eee1abd0b35e6eb617229796080fbd
jegedenifemi <jegedenifemi24@gmail.com>
2022-09-10 00:02:03 +0100

    retain selected theme

--- index.js
+++ index.js
@@ -20,13 +20,33 @@
 // })
 // console.log('textarea','textarea')
 
-
-
-
-
 // THEME CHANGE ON DROPDOWN SELECT
-const setTheme = (theme) => (document.documentElement.className = theme);
+// const setTheme = (theme) => (document.documentElement.className = theme);
 
-document.getElementById("theme_select").addEventListener("change", function () {
+// document.getElementById("theme_select").addEventListener("change", function () {
+//   setTheme(this.value);
+// });
+
+// SET THEME BY GIVING ROOT CLASS NAME
+let setTheme = (theme) => (document.documentElement.className = theme);
+
+// ASSIGN DROP DOWN TO A VARIABLE
+const themeDropDown = document.getElementById("theme_select");
+
+// SET THEME ON SELECT OF DROPDOWN OPTION
+themeDropDown.addEventListener("change", function () {
   setTheme(this.value);
+
+  // STORE SELECTED THEME ON LOCAL STORAGE
+  localStorage.setItem("theme", themeDropDown.value);
 });
+
+// GET STORED THEME
+const storedTheme = localStorage.getItem("theme");
+
+// APPLY THEME WHEN WINDOW LOADS
+window.onload = () => {
+  if (storedTheme !== null) {
+    setTheme(storedTheme);
+  }
+};

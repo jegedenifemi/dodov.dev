d0612dd1ecf3648150829d0bc4fbeaed0f237b79
jegedenifemi <jegedenifemi24@gmail.com>
2022-09-10 15:43:54 +0100

    favicon retained on page change

--- index.js
+++ index.js
@@ -34,61 +34,58 @@ themeDropDown.addEventListener("change", function () {
   localStorage.setItem("theme", themeDropDown.value);
 });
 
-// GET STORED THEME
+// GET STORED THEME & APPLY WHEN NEW PAGE LOADS
 const storedTheme = localStorage.getItem("theme");
+window.onload = setTheme(storedTheme);
 
-// APPLY THEME WHEN WINDOW LOADS
-window.onload = () => {
-  if (storedTheme !== null) {
-    setTheme(storedTheme);
+// FUNCTION TO REPLACE FAVICON LINK IN HTML HEAD
+function changeFavicon(src) {
+  var newLink = document.createElement("link");
+  var oldLink = document.getElementById("favicon");
+  newLink.id = "favicon";
+  newLink.rel = "shortcut icon";
+  newLink.href = src;
+  // DELETE OLD FAVICON IN HEAD
+  if (oldLink) {
+    document.head.removeChild(oldLink);
   }
-};
+  // ADD NEW FAVICON TO HEAD
+  document.head.appendChild(newLink);
+
+  // KEEP SAME FAVICON EVEN ON A DIFFERENT PAGE
+  localStorage.setItem("icon", newLink.href);
+}
 
 // CHANGE FAVICON WITH SELECTED THEME
 themeDropDown.onchange = () => {
-  function changeFavicon(src) {
-    var newLink = document.createElement("link");
-    var oldLink = document.getElementById("favicon");
-    newLink.id = "favicon";
-    newLink.rel = "icon";
-    newLink.href = src;
-    // DELETE OLD FAVICON IN HEAD
-    if (oldLink) {
-      document.head.removeChild(oldLink);
-    }
-    // ADD NEW FAVICON TO HEAD
-    document.head.appendChild(newLink);
-  }
-
   // CHANGE FAVICON LINK SRC DEPENDING ON THEME SELECTED
-  if (themeDropDown.value == "dark") {
+  if (themeDropDown.value === "dark") {
     changeFavicon(
       "https://dodov.dev/media/site/7c35252aa6-1640615600/favicon-dark-plus.png"
     );
-  } else if (themeDropDown.value == "light") {
+  } else if (themeDropDown.value === "light") {
     changeFavicon(
       "https://dodov.dev/media/site/9c26eb7f74-1614418428/favicon-light-plus.png"
     );
-  } else if (themeDropDown.value == "monokai") {
+  } else if (themeDropDown.value === "monokai") {
     changeFavicon(
       "https://dodov.dev/media/site/d0298ef0fd-1614418428/favicon-monokai.png"
     );
-  } else if (themeDropDown.value == "g-dark") {
+  } else if (themeDropDown.value === "g-dark") {
     changeFavicon(
       "https://dodov.dev/media/site/f2f7858efb-1640615600/favicon-github-dark.png"
     );
-  } else if (themeDropDown.value == "g-light") {
+  } else if (themeDropDown.value === "g-light") {
     changeFavicon(
       "https://dodov.dev/media/site/381999d79e-1640615600/favicon-github-light.png"
     );
-  } else if (themeDropDown.value == "solarized") {
+  } else if (themeDropDown.value === "solarized") {
     changeFavicon(
       "https://dodov.dev/media/site/b038a44974-1640615600/favicon-solarized-light.png"
     );
   }
-
-  // KEEP SAME FAVICON EVEN ON A DIFFERENT PAGE
-  localStorage.setItem("icon", newLink.href);
-  const storedIcon = localStorage.getItem("icon");
-  window.onload = changeFavicon(storedIcon);
 };
+
+// GET STORED FAVICON & APPLY WHEN NEW PAGE LOADS
+const storedIcon = localStorage.getItem("icon");
+window.onload = changeFavicon(storedIcon);
